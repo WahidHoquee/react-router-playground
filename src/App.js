0e5a182e6e91156460e0ac8f2ed137ddb74d6fd5
Wahid Hoque <wahidhoquee@gmail.com>
2021-02-05 03:36:08 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
 
-function App() {
+import Navigation from './nav';
+import About from './pages/About';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <div className="">
+        <Navigation/>
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/shop" component={Shop} />         
+        </Switch>
+      </div>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <nav className="menu">
+      <h2>Logo</h2>
+      <ul className="links">
+        <li>About</li>
+        <li>Shop</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
